104de6cf6f251677d6ddd470abb6bf917395eb6a
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-28 18:32:46 -0500

    add login desing

--- app/src/main/java/com/example/looptser/MainActivity.java
+++ app/src/main/java/com/example/looptser/MainActivity.java
@@ -5,18 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button testBtn;
+    private TextView testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testBtn = findViewById(R.id.testBtn);
+        testBtn = findViewById(R.id.login_register);
 
         testBtn.setOnClickListener(new View.OnClickListener() {
             @Override
