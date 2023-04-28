37cf2f0ec726a37a7a00c03696fca8c52b9196bc
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-27 21:58:50 -0500

    add navigation between fragments

---
+++ app/src/main/java/com/example/looptser/HomeActivity.java
@@ -0,0 +1,56 @@
+package com.example.looptser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.looptser.Fragments.Chat_Fragment;
+import com.example.looptser.Fragments.Home_Fragment;
+import com.example.looptser.Fragments.Notification_Fragment;
+import com.example.looptser.Fragments.Profile_Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        bottomNavigationView = findViewById(R.id.bottonnav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new Home_Fragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.dashbord:
+                fragment = new Home_Fragment();
+                break;
+            case R.id.notification:
+                fragment = new Notification_Fragment();
+                break;
+            case R.id.chat:
+                fragment = new Chat_Fragment();
+                break;
+            case R.id.profile:
+                fragment = new Profile_Fragment();
+                break;
+        }
+
+        if (fragment != null) {
+            loadFragment(fragment);
+        }
+        return true;
+    }
+    void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.relativelayout, fragment).commit();
+    }
+}
