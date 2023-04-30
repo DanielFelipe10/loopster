988800fee936fafdd939aa4801f4bc9340d69846
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-29 20:44:31 -0500

    min changes

--- app/src/main/java/com/example/looptser/MainActivity.java
+++ app/src/main/java/com/example/looptser/MainActivity.java
@@ -5,24 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView testBtn;
+    private Button openPrivateActivities;
+    private TextView openSignupActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testBtn = findViewById(R.id.login_register);
+        openSignupActivity = findViewById(R.id.login_register);
+        openPrivateActivities = findViewById(R.id.login_action_btn);
 
-        testBtn.setOnClickListener(new View.OnClickListener() {
+        openSignupActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        openPrivateActivities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Here goes the authentication and redirection
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
