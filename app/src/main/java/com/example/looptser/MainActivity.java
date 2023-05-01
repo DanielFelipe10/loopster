9b69ef140f2d22c3bacdf3a5dd28a4d0a246dc10
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-01 12:42:04 -0500

    add login and signup logic

--- app/src/main/java/com/example/looptser/MainActivity.java
+++ app/src/main/java/com/example/looptser/MainActivity.java
@@ -1,25 +1,52 @@
 package com.example.looptser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.looptser.auth.Auth;
+import com.example.looptser.utils.DialogError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+
     private Button openPrivateActivities;
     private TextView openSignupActivity;
+
+    private EditText userEmail;
+    private EditText userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Auth auth = new Auth();
+        DialogError dialog = new DialogError();
+
+        mAuth = FirebaseAuth.getInstance();
+
         openSignupActivity = findViewById(R.id.login_register);
         openPrivateActivities = findViewById(R.id.login_action_btn);
+
+        userEmail = findViewById(R.id.login_user_input);
+        userPassword = findViewById(R.id.login_password_input);
 
         openSignupActivity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,11 +59,58 @@ public class MainActivity extends AppCompatActivity {
         openPrivateActivities.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Here goes the authentication and redirection
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
+                String email = userEmail.getText().toString().trim();
+                String password = userPassword.getText().toString().trim();
+
+                if (auth.validateSigninData(email, password)) {
+                        signIn(email, password);
+                    } else {
+                        dialog.show(getSupportFragmentManager(), "Datos invalidos");
+                }
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
+    private void reload() {
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
