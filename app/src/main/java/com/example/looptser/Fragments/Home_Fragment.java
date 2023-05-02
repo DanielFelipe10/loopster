b836b064ad8d97f57c707639f180743e5ce9d3f7
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-01 20:48:15 -0500

    add google provider login

--- app/src/main/java/com/example/looptser/Fragments/Home_Fragment.java
+++ app/src/main/java/com/example/looptser/Fragments/Home_Fragment.java
@@ -1,6 +1,5 @@
 package com.example.looptser.Fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,35 +12,16 @@ import android.widget.Button;
 import com.example.looptser.R;
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Home_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Home_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public Home_Fragment() {
-        // Required empty public constructor
-    }
+    public Home_Fragment() {}
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Home_Fragment newInstance(String param1, String param2) {
         Home_Fragment fragment = new Home_Fragment();
         Bundle args = new Bundle();
@@ -61,9 +41,7 @@ public class Home_Fragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_, container, false);
 
         FirebaseAuth fAuth = FirebaseAuth.getInstance();
