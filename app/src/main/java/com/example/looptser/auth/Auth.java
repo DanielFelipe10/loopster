591fd35836a20d7394da450e3a692a6db0535746
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-09 17:08:10 -0500

    fix email regex add to all the methods

--- app/src/main/java/com/example/looptser/auth/Auth.java
+++ app/src/main/java/com/example/looptser/auth/Auth.java
@@ -3,17 +3,21 @@ package com.example.looptser.auth;
 import java.util.Objects;
 
 public class Auth {
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
     public boolean validateSignupData(String name, String email, String password, String confirmPassword) {
-        if(!Objects.equals(name, "") && !Objects.equals(email, "") && !Objects.equals(password, "") && !Objects.equals(confirmPassword, "")) {
-            return password.equals(confirmPassword);
+        if(email.matches(emailPattern)) {
+            if(!Objects.equals(name, "") && !Objects.equals(email, "") && !Objects.equals(password, "") && !Objects.equals(confirmPassword, "")) {
+                return password.equals(confirmPassword);
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
     }
 
     public boolean validateSigninData(String name, String email) {
-        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-
         if(email.matches(emailPattern)) {
             return !Objects.equals(name, "") && !Objects.equals(email, "");
         } else {
