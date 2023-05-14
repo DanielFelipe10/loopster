d27b173371df699cd79b9d2ba8f16005c2c8507c
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-14 13:44:15 -0500

    add chat recycle view adapter

---
+++ app/src/main/java/com/example/looptser/MyAdapter.java
@@ -0,0 +1,61 @@
+package com.example.looptser;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.looptser.users.Users;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+//Get the actual context and reuse the user_item_row.xml file
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<Users> usersArrayList;
+
+    public MyAdapter(Context context, ArrayList<Users> usersArrayList) {
+        this.context = context;
+        this.usersArrayList = usersArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Load the user_item_row.xml layout to the context
+        View v = LayoutInflater.from(context).inflate(R.layout.user_item_row, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        //Load the data from the array to each user_item_row.xml
+        Users users = usersArrayList.get(position);
+        holder.userName.setText(users.getName());
+        Picasso.get().load(users.getImgUriProfile()).into(holder.userImg);
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersArrayList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView userImg;
+        TextView userName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userImg = itemView.findViewById(R.id.user_image);
+            userName = itemView.findViewById(R.id.user_name);
+        }
+    }
+}
